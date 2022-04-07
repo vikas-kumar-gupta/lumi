d85febffb166cc219cf802f0d2b780d666784a19
vikas-kumar-gupta <vikas.vg118@gmail.com>
2022-04-07 13:33:18 +0530

    jwt middleware

---
+++ src/middlewares/user.middleware.ts
@@ -0,0 +1,38 @@
+import { CONFIG, STATUS_MSG } from '../constants'
+import express, { Request, Response, NextFunction, Application } from 'express'
+import jwt from 'jsonwebtoken';
+
+
+export const isLoggedIn = async (req: express.Request, res: express.Response, next: NextFunction) => {
+    try {
+        const token = req.cookies.jwt;
+        if (token != undefined) {
+            const verifyToken = jwt.verify(token, CONFIG.JWT_SECRET_KEY, (err: any, data: any) => {
+                if (err) throw new Error(STATUS_MSG.ERROR.TOKEN_ALREADY_EXPIRED.message)
+                req.body.tokenId = data._id;   
+            })
+        }
+        else {
+            throw new Error(STATUS_MSG.ERROR.TOKEN_ALREADY_EXPIRED.message)
+        }
+    }
+    catch (err) {
+        res.status(STATUS_MSG.ERROR.INVALID_TOKEN.statusCode).json(STATUS_MSG.ERROR.INVALID_TOKEN)
+    }
+}
+
+export const auth = async (req: express.Request, res: express.Response, next: NextFunction) => {
+    try {
+        const token = req.cookies.jwt;
+        const verifyToken = jwt.verify(token, CONFIG.JWT_SECRET_KEY, (err: any, data: any) => {
+            if (err) {
+                throw new Error(err)
+            }
+            req.body.tokenId = data._id;
+        })
+        next();
+    }
+    catch (err) {
+        res.status(STATUS_MSG.ERROR.BAD_REQUEST.statusCode).json(STATUS_MSG.ERROR.BAD_REQUEST)
+    }
+}
